d0c1fb0864548355ca1be3a7560f5f6ef5479146
Quim Picó Mora <quim@pop-os.localdomain>
2019-10-05 16:43:17 +0200

    google extension per a testejar

--- extension/TODO1.js
+++ extension/TODO1.js
@@ -1,3 +1,17 @@
-chrome.runtime.onInstalled.addListener(()=> {
-    console.log("APP IN TWITTER");
-});
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+      console.log('The color is green.');
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostEquals: 'twitter.com'},
+        }),
+        new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {hostEquals: 'www.reddit.com'},
+        })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+    });
+  });
