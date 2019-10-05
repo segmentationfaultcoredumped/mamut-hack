1a43d4bc0f8d8409e2d56eb5b55659f0653825ee
JoanPalau <joanp4l4u@gmail.com>
2019-10-05 21:07:43 +0200

    feat: added scroll and onload actions

--- extension/twitter-hate.js
+++ extension/twitter-hate.js
@@ -1,1 +1,40 @@
-console.log("WORKSSSSS IN TWITTER");
+window.addEventListener ("load", myMain, false);
+
+let scrolling = false;
+ 
+window.addEventListener('scroll', function() {
+  scrolling = true;
+}, false);
+ 
+setInterval( function() {
+  if ( scrolling ) {
+    scrolling = false;
+    myMain()
+  }
+}, 1200 );
+ 
+
+function myMain (evt) {
+
+    let jsInitChecktimer = setInterval (checkForJS_Finish, 111);
+
+    function checkForJS_Finish () {
+        if ( document.querySelector ("article")) {
+            clearInterval (jsInitChecktimer);
+            // DO YOUR STUFF HERE.
+            let tweets = document.getElementsByTagName('article');
+            for (tweet of tweets){
+                // try .remove on element
+                //add if for testing if hate speach or not
+                tweet.classList.add('hidden-hate');
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
